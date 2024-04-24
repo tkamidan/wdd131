d253fd5d6eb051f072feba7daa238cae58584878
tkamidan <tkamidan@outlook.com>
2024-04-24 17:29:40 -0600

    Did the ponder activity

---
+++ w01ponder/activity2/script.js
@@ -0,0 +1,7 @@
+const myApples = 4;
+const friendApples = 2;
+let total = myApples + friendApples;
+
+document.getElementById("myApples").textContent = myApples;
+document.getElementById("friendApples").textContent = friendApples;
+document.getElementById("total").textContent = total;
