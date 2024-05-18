f7222cea7e36e0e71f23d9ed71d8ff6266c83bf9
tkamidan <tkamidan@outlook.com>
2024-05-18 12:35:54 -0600

    added Menu drop down, tried to add viewer but failed

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,17 +1,57 @@
-function addMenuText() {
-    var menuText = document.querySelector('#menu-title');
-    menuText.style.display = 'none';
+function toggleMenu() {
+    var links = document.getElementsByClassName('links');
+    for (var i = 0; i < links.length; i++) {
+        if (links[i].style.display === 'none') {
+            links[i].style.display = 'grid';
+        } else {
+            links[i].style.display = 'none';
+        }
+    }
 }
 
-function removeMenuText() {
+function showMenu() {
+    var menuText = document.querySelector('#menu-title');
+    menuText.style.display = 'none';
+    var links = document.querySelector('.links');
+    links.style.display = 'grid';
+}
+
+function hideMenu() {
     var menuText = document.querySelector('#menu-title');
     menuText.style.display = 'block';
+    var links = document.querySelector('.links');
+    links.style.display = 'none';
 }
 
 window.addEventListener('resize', function () {
     if (this.window.innerWidth >= 700) {
-        addMenuText();
+        showMenu();
     } else {
-        removeMenuText();
+        hideMenu();
     }
 })
+
+var imgList = document.querySelector('.gallery');
+var viewer = document.querySelector('#viewer');
+var imgViewer = document.querySelector('.img-viewer img');
+
+imgList.forEach(function(img) {
+    img.addEventListener('click', function() {
+        console.log('Image clicked: ', img.src);
+        openViewer(img.src);
+    });
+});
+
+
+function changeImage(imageURL) {
+    imgViewer.src = imageURL;
+}
+
+function openViewer(imageURL) {
+    viewer.style.display = 'grid';
+    changeImage(imageURL);
+}
+
+function closeViewer() {
+    viewer.style.display = 'none';
+}
