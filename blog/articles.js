2d633009d414121a19118ce693a1f48a41be6442
tkamidan <tkamidan@outlook.com>
2024-05-29 16:20:24 -0600

    Added javascript to add the individual articles

--- blog/articles.js
+++ blog/articles.js
@@ -9,7 +9,7 @@ const articles = [
     imgAlt: "Book cover for Septimus Heap 1",
     ages: "10-14",
     genre: "Fantasy",
-    stars: "****"
+    stars: "⭐⭐⭐⭐"
   },
   {
     id: 2,
@@ -23,5 +23,54 @@ const articles = [
     ages: "12-16",
     genre: "Fantasy",
     stars: "⭐⭐⭐⭐"
+  },
+  {
+    id: 3,
+    title: "Diary of a Wimpy Kid",
+    date: "April 1, 2007",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque cupiditate id libero veritatis laborum delectus ullam saepe atque eius ipsam, in molestias doloribus deserunt rerum totam quos illo consequatur obcaecati.",
+    imgSrc: "./diary_of_a_wimpy_kid.jpg",
+    imgAlt: "diary_of_a_wimpy_kid",
+    ages: "8-12",
+    genre: "Realistic Fiction",
+    stars: "⭐⭐⭐⭐⭐"
+  },
+  {
+    id: 4,
+    title: "Diary of a Wimpy Kid: Rodrick Rules",
+    date: "February 1, 2008",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque cupiditate id libero veritatis laborum delectus ullam saepe atque eius ipsam, in molestias doloribus deserunt rerum totam quos illo consequatur obcaecati.",
+    imgSrc: "./dowk2jpg.jpg",
+    imgAlt: "diary_of_a_wimpy_kid_rodrick_rules",
+    ages: "8-12",
+    genre: "Realistic Fiction",
+    stars: "⭐⭐⭐⭐⭐"
   }
 ];
+
+function articleTemplate(article) {
+  const { title, date, description, imgSrc, imgAlt, ages, genre, stars } = article;
+  return `
+    <article> 
+      <figure class="right">
+        <h2>${title}</h2> 
+        <img src="${imgSrc}" alt="${imgAlt}"> 
+        <p>${description}</p>
+      </figure>
+      <figure class="left">
+        <h3>${date}</h3>
+        <h4>${ages}</h4>
+        <h4>${genre}</h4>
+        <h4>${stars}</h4> 
+      </figure>
+    </article>
+  `
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  var articleSection = document.querySelector("#articles");
+  articles.forEach(article => {
+   var articleHTML = articleTemplate(article);
+    articleSection.insertAdjacentHTML("beforeend", articleHTML);
+  });
+});
