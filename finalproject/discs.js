f5cb9ac22f96a2ed08966573b8679e3770bbe0af
tkamidan <tkamidan@outlook.com>
2024-07-20 13:53:19 -0600

    added more changes

---
+++ finalproject/discs.js
@@ -0,0 +1,14 @@
+function discTemplate(imgURL, imgAlt, discName, speed, glide, turn, fade, stability, description) {
+    return `<div class="disc-section">
+                <img src="${imgURL}" alt="${imgAlt}">
+                <div class="text-section">
+                    <h2>${discName}</h2>
+                    <h3>Speed: ${speed}</h3>
+                    <h3>Glide: ${glide}</h3>
+                    <h3>Turn: ${turn}</h3>
+                    <h3>Fade: ${fade}</h3>
+                    <h3>Stability: ${stability}</h3>
+                    <p>${description}</p>
+                </div>
+            </div>`;
+}
