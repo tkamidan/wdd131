b430345fd5e24e1148f64a6b3b14761a056ee624
tkamidan <tkamidan@outlook.com>
2024-07-20 14:18:33 -0600

    javascript changes

--- finalproject/discs.js
+++ finalproject/discs.js
@@ -3,11 +3,11 @@ function discTemplate(imgURL, imgAlt, discName, speed, glide, turn, fade, stabil
                 <img src="${imgURL}" alt="${imgAlt}">
                 <div class="text-section">
                     <h2>${discName}</h2>
+                    <h3>Disc Type: 
                     <h3>Speed: ${speed}</h3>
                     <h3>Glide: ${glide}</h3>
                     <h3>Turn: ${turn}</h3>
                     <h3>Fade: ${fade}</h3>
-                    <h3>Stability: ${stability}</h3>
                     <p>${description}</p>
                 </div>
             </div>`;
--- finalproject/discs.mjs
+++ finalproject/discs.mjs
@@ -3,14 +3,22 @@ const discs = [
         discName: 'Buzzz',
         imgURL: './images/Buzzz26b.webp',
         imgAlt: 'Buzzz',
+        discType: 'Mid-Range'
         speed: '5',
         slide: '4',
         turn: '1',
         fade: '1',
-        stability: '0.5',
         description: "The Buzzz disc is a popular midrange disc golf disc manufactured by Discraft. Known for its versatility and reliability, the Buzzz offers a stable flight path, making it a go-to choice for players of all skill levels. It features a smooth, consistent glide and is designed to handle various throwing styles and power levels. The Buzzz's ability to hold any line you put it on, whether for straight shots, hyzers, or anhyzers, has made it a staple in many disc golfers' bags. Its reliability in various wind conditions and its comfortable grip contribute to its reputation as one of the most trusted discs in the sport"
     },
     {
-        discName: ''
+        discName: 'Innova Mamba',
+        imgURL: './images/mamba.jpg',
+        imgAlt: 'Innova Mamba',
+        discType: 'Distance Driver',
+        speed: '11',
+        glide: '6', 
+        turn: '-5',
+        fade: '1',
+        description: "The Innova Mamba is a high-speed understable distance driver designed to provide maximum distance for players with moderate arm speed. It is particularly suited for tailwind shots, long anhyzer lines, and rollers. The Mamba's understable nature makes it an excellent choice for beginners seeking to achieve longer throws and for advanced players looking for controlled turnover shots. With its significant turn and reliable glide, the Mamba excels at creating s-curves and extended flights, helping players achieve greater distance with less effort. Its comfortable grip and predictable flight pattern make it a versatile addition to any disc golfer's bag."
     }
 ]
