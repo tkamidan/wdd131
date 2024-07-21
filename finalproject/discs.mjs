36f50db4d79dd2ea390e945677459729e1ed8b3d
tkamidan <tkamidan@outlook.com>
2024-07-20 18:24:14 -0600

    completed file

--- finalproject/discs.mjs
+++ finalproject/discs.mjs
@@ -1,14 +1,14 @@
 const discs = [
     {
-        discName: 'Buzzz',
+        discName: 'Discraft Buzzz',
         imgURL: './images/Buzzz26b.webp',
-        imgAlt: 'Buzzz',
-        discType: 'Mid-Range'
+        imgAlt: 'Discraft Buzzz',
+        discType: 'Midrange',
         speed: '5',
-        slide: '4',
+        glide: '4',
         turn: '1',
         fade: '1',
-        description: "The Buzzz disc is a popular midrange disc golf disc manufactured by Discraft. Known for its versatility and reliability, the Buzzz offers a stable flight path, making it a go-to choice for players of all skill levels. It features a smooth, consistent glide and is designed to handle various throwing styles and power levels. The Buzzz's ability to hold any line you put it on, whether for straight shots, hyzers, or anhyzers, has made it a staple in many disc golfers' bags. Its reliability in various wind conditions and its comfortable grip contribute to its reputation as one of the most trusted discs in the sport"
+        description: "The Buzzz disc is a popular midrange disc golf disc manufactured by Discraft. Known for its versatility and reliability, the Buzzz offers a stable flight path, making it a go-to choice for players of all skill levels. It features a smooth, consistent glide and is designed to handle various throwing styles and power levels. The Buzzz's ability to hold any line you put it on, whether for straight shots, hyzers, or anhyzers, has made it a staple in many disc golfers' bags. Its reliability in various wind conditions and its comfortable grip contribute to its reputation as one of the most trusted discs in the sport."
     },
     {
         discName: 'Innova Mamba',
@@ -16,9 +16,44 @@ const discs = [
         imgAlt: 'Innova Mamba',
         discType: 'Distance Driver',
         speed: '11',
-        glide: '6', 
+        glide: '6',
         turn: '-5',
         fade: '1',
         description: "The Innova Mamba is a high-speed understable distance driver designed to provide maximum distance for players with moderate arm speed. It is particularly suited for tailwind shots, long anhyzer lines, and rollers. The Mamba's understable nature makes it an excellent choice for beginners seeking to achieve longer throws and for advanced players looking for controlled turnover shots. With its significant turn and reliable glide, the Mamba excels at creating s-curves and extended flights, helping players achieve greater distance with less effort. Its comfortable grip and predictable flight pattern make it a versatile addition to any disc golfer's bag."
+    },
+    {
+        discName: 'Axiom Envy',
+        imgURL: './images/envy.png',
+        imgAlt: 'Axiom Envy',
+        discType: 'Putter',
+        speed: '3',
+        glide: '3',
+        turn: '0',
+        fade: '2',
+        description: "The Axiom Envy is the best rated putter in disc golf. It is one of the most loved putters in the sport. It comes in a variety of different colors as well."
+    },
+    {
+        discName: 'Innova Panther',
+        imgURL: './images/panther.jpg',
+        imgAlt: 'Innova Panther',
+        discType: 'Midrange',
+        speed: '5',
+        glide: '4',
+        turn: '-2',
+        fade: '1',
+        description: "The Innova Panther is a versatile midrange disc designed for precision and control. It features a comfortable rim grip and is known for its reliable glide and smooth turnover flight path. The Panther is an excellent choice for players of all skill levels, especially for those looking to execute accurate approach shots, hyzer flips, and anhyzer lines. Its stability allows it to handle both forehand and backhand throws effectively, making it a go-to disc for navigating tight fairways and achieving pinpoint accuracy on the course."
+    },
+    {
+        discName: 'Innova Destroyer',
+        imgURL: './images/destroyerchamp.png',
+        imgAlt: 'Innova Destroyer',
+        discType: 'Distance Driver',
+        speed: '12',
+        glide: '5',
+        turn: '-1',
+        fade: '3',
+        description: "The Innova Destroyer is a high-speed, overstable distance driver renowned for its combination of power and precision. With a speed rating of 12, glide of 5, turn of -1, and fade of 3, the Destroyer is designed for long-distance throws, excelling in both headwinds and calm conditions. Its stability makes it a favorite among advanced and professional players for achieving significant distance with reliable fade. The Destroyer's versatility allows for powerful backhand and forehand throws, making it a staple in many disc golfers' bags. Its consistent performance and adaptability to various throwing styles contribute to its reputation as one of the top distance drivers in the sport."
     }
 ]
+
+export default discs;
